94dcdab1e9c40d329ddda0734eaa5c163b5659c7
Ivan Yelizariev <ivann.exe@gmail.com>
2014-07-05 14:22:36 +0600

    update oauth_provider description

--- oauth_provider/__openerp__.py
+++ oauth_provider/__openerp__.py
@@ -1,49 +1,76 @@
 {
-    'name' : 'OAuth2 provider (server)',
+    'name' : 'OAuth2 server',
     'version' : '1.0.0',
     'author' : 'Ivan Yelizariev',
     'category' : 'Base',
     'website' : 'https://it-projects.info',
-    'sequence': 1,
 
     'depends' : ['web'],
     'data':[],
     'installable': True,
 
     'description': '''
-Addon allows to customers to login in their databases via res_users records in your main database
+Addon allows to users to login in openerp databases via master database
 
 Depends on https://github.com/idan/oauthlib
 
-Basic flow:
+Basic flow
+==========
 
-Open url1 at master server from child database:
+Terms
+-----
+
+* User -- a human
+* Master -- an openerp database where this module is installed
+* Client -- an openerp database where you need to authenticate user and where module auth_oauth module is installed
+
+
+Steps
+-----
+
+From client database redirects user to url1 at master database:
 
     import urllib
 
-    url1 = '/oauth2/auth?%s'%urllib.urlencode({
-        'state': {"p": 1, "r": "%2Fweb%2Flogin%3F", "d": "some-test-3"},
-        'redirect_uri': 'http://odoo.example.com/auth_oauth/signin',
-        'response_type': 'token',
-        'client_id': 'f23514a1-13a2-40e5-9033-5018e7f3a052',
-        'debug': True,
-        'scope': 'userinfo'
-        })
+    url1 = '/oauth2/auth?%s' % urllib.urlencode({
+
+    'state': {"p": 1, "r": "%2Fweb%2Flogin%3F", "d": "some-test-3"},
+
+    'redirect_uri': 'http://odoo.example.com/auth_oauth/signin',
+
+    'response_type': 'token',
+
+    'client_id': 'f23514a1-13a2-40e5-9033-5018e7f3a052',
+
+    'debug': False,
+
+    'scope': 'userinfo'
+
+    })
 
     print url1
 
-then user inter login-password
+if user is not logined at master:
 
-then master back:
+* master redirects user to /web/login page
+* user enters login-password
+* master redirects user to url1
+
+then master redirects user back:
 
     http://odoo.example.com/auth_oauth/signin#access_token=ksESez4jRUxg0v6Kt7HkE5Z4ZtyrrM&token_type=Bearer&state=%7B%27p%27%3A+1%2C+%27r%27%3A+%27%252Fweb%252Flogin%253F%27%2C+%27d%27%3A+%27some-test-3%27%7D&expires_in=3600&scope=userinfo
 
-then client redirect to itself (see @fragment_to_query_string in auth_oauth module)
+then client redirects user to itself (see @fragment_to_query_string in auth_oauth module)
 
     http://odoo.example.com/auth_oauth/signin?access_token=ksESez4jRUxg0v6Kt7HkE5Z4ZtyrrM&token_type=Bearer&state=%7B%27p%27%3A+1%2C+%27r%27%3A+%27%252Fweb%252Flogin%253F%27%2C+%27d%27%3A+%27some-test-3%27%7D&expires_in=3600&scope=userinfo
 
 then client make request to master:
 
     /oauth2/tokeninfo?access_token=ksESez4jRUxg0v6Kt7HkE5Z4ZtyrrM
+
+and master returns user data:
+
+    {"user_id": 3, "email": "admin@example.com", "name": "Administrator"}
+
     ''',
 }

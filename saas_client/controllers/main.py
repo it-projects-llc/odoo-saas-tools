c0928a8819c6daea12dd3eddfe4966c99b07632c
Cesar Lage <kaerdsar@gmail.com>
2015-03-20 13:09:13 -0400

    fix bug in saas_client

--- saas_client/controllers/main.py
+++ saas_client/controllers/main.py
@@ -11,17 +11,3 @@ class SaasClient(http.Controller):
     def new_database(self, **post):
         params = werkzeug.url_encode(post)
         return werkzeug.utils.redirect('/auth_oauth/signin?%s' % params)
-
-
-class OAuthLogin(oauth.OAuthLogin):
-
-    @http.route()
-    def web_login(self, *args, **kw):
-        if kw.get('login', False):
-            user = request.registry.get('res.users')
-            domain = [('login', '=', kw['login'])]
-            fields = ['share', 'database']
-            data = user.search_read(request.cr, SUPERUSER_ID, domain, fields)
-            if data and data[0]['share'] and data[0]['database']:
-                kw['redirect'] = '/saas_server/tenant'
-        return super(OAuthLogin, self).web_login(*args, **kw)

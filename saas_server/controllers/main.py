72eb6cb9869b4ffcd480d688d89e275dd667ce8a
José Andrés Hernández Bustio <jbustio@gmail.com>
2015-03-30 10:04:59 -0400

    Validate domain name in sign up form

--- saas_server/controllers/main.py
+++ saas_server/controllers/main.py
@@ -7,6 +7,7 @@ from openerp.addons.web.http import request
 from openerp.addons.auth_oauth.controllers.main import fragment_to_query_string
 from openerp.addons.web.controllers.main import db_monodb
 from openerp.addons.saas_utils import connector
+import re
 
 import werkzeug.utils
 import simplejson
@@ -202,6 +203,7 @@ class AuthSignupHome(auth_signup.controllers.main.AuthSignupHome):
             f_dbname = '%s.%s' % (qcontext['dbname'], self.get_saas_domain())
             full_dbname = f_dbname.replace('www.', '').replace('.', '_')
             db_exists = openerp.service.db.exp_db_exist(full_dbname)
+            assert re.match('[a-zA-Z0-9_.-]+$'== False, values.get('dbname')), "The domain name can only contain letters or numbers"
             assert db_exists == False, "Domain exists"
         assert any([k for k in values.values()]), "The form was not properly filled in."
         assert values.get('password') == qcontext.get('confirm_password'), "Passwords do not match; please retype them."

a154bfb3493c715e0067001b01dbb842ac904f53
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-29 20:24:21 +0200

    [REF] move database preparing stuff to saas_server.client model for easier inheritance

--- saas_server/controllers/main.py
+++ saas_server/controllers/main.py
@@ -34,70 +34,10 @@ class SaasServer(http.Controller):
             raise Exception(admin_data['error'])
         client_id = admin_data.get('client_id')
 
-        if template_db:
-            openerp.service.db._drop_conn(request.cr, template_db)
-            #openerp.service.db.exp_drop(new_db) # for debug
-            openerp.service.db.exp_duplicate_database(template_db, new_db)
-        else:
-            demo = state.get('demo')
-            lang = state.get('lang') or 'en_US'
-            openerp.service.db.exp_create_database(new_db, demo, lang)
+        context = state.copy()
 
-        registry = openerp.modules.registry.RegistryManager.get(new_db)
-
-        with registry.cursor() as cr:
-            # update database.uuid
-            registry['ir.config_parameter'].set_param(cr, SUPERUSER_ID,
-                                                      'database.uuid',
-                                                      client_id)
-            #STOPHERE: fix for new template database
-            # save auth data
-            oauth_provider_data = {'enabled': False, 'client_id': client_id}
-            for attr in ['name', 'auth_endpoint', 'scope', 'validation_endpoint', 'data_endpoint', 'css_class', 'body']:
-                oauth_provider_data[attr] = getattr(saas_oauth_provider, attr)
-            oauth_provider_id = registry['auth.oauth.provider'].create(cr, SUPERUSER_ID, oauth_provider_data)
-            registry['ir.model.data'].create(cr, SUPERUSER_ID, {
-                'name': 'saas_oauth_provider',
-                'module': 'saas_server',
-                'noupdate': True,
-                'model': 'auth.oauth.provider',
-                'res_id': oauth_provider_id,
-            })
-            # 1. Update company with organization
-            vals = {'name': organization}
-            registry['res.company'].write(cr, SUPERUSER_ID, 1, vals)
-            partner = registry['res.company'].browse(cr, SUPERUSER_ID, 1)
-            registry['res.partner'].write(cr, SUPERUSER_ID, partner.id,
-                                          {'email': admin_data['email']})
-            # 2. Update user credentials
-            domain = [('login', '=', template_db)]
-            user_ids = registry['res.users'].search(cr, SUPERUSER_ID, domain)
-            user_id = user_ids and user_ids[0] or SUPERUSER_ID
-            user = registry['res.users'].browse(cr, SUPERUSER_ID, user_id)
-            user.write({
-                'login': admin_data['email'],
-                'name': admin_data['name'],
-                'email': admin_data['email'],
-                'parent_id': partner.id,
-                'oauth_provider_id': oauth_provider_id,
-                'oauth_uid': admin_data['user_id'],
-                'oauth_access_token': access_token
-            })
-            # 3. Set suffix for all sequences
-            seq_ids = registry['ir.sequence'].search(cr, SUPERUSER_ID,
-                                                     [('suffix', '=', False)])
-            suffix = {'suffix': client_id.split('-')[0]}
-            registry['ir.sequence'].write(cr, SUPERUSER_ID, seq_ids, suffix)
-            # get action_id
-            action_id = registry['ir.model.data'].xmlid_to_res_id(cr, SUPERUSER_ID, action)
-
-            # 4. install addons
-            addons = state.get('addons')
-            if addons:
-                addon_ids = registry['ir.module.module'].search(cr, SUPERUSER_ID, [('name', 'in', addons)])
-                for addon_id in addon_ids:
-                    registry['ir.module.module'].button_immediate_install(cr, SUPERUSER_ID, addon_id)
-
+        client_data = {'name':new_db, 'client_id': client_id}
+        client = request.env['saas_server.client'].with_context(**context).create(client_data)
         params = {
             'access_token': post['access_token'],
             'state': simplejson.dumps({
--- saas_server/models/saas_server.py
+++ saas_server/models/saas_server.py
@@ -1,7 +1,7 @@
 import os
 import openerp
 from openerp import SUPERUSER_ID
-from openerp import models, fields
+from openerp import models, fields, Environment
 from openerp.addons.saas_utils import connector, database
 
 
@@ -31,6 +31,83 @@ class SaasServerClient(models.Model):
                               ('pending','Pending'),
                               ('deleted','Deleted')],
                              'State', default='open', track_visibility='onchange')
+
+    def create(self, vals):
+        template_db = self._context.get('template_db')
+        if template_db:
+            openerp.service.db._drop_conn(self.env.cr, template_db)
+            #openerp.service.db.exp_drop(new_db) # for debug
+            openerp.service.db.exp_duplicate_database(template_db, self.name)
+        else:
+            demo = self._context.get('demo')
+            lang = self._context.get('lang') or 'en_US'
+            openerp.service.db.exp_create_database(new_db, demo, lang)
+        res = super(SaasServerClient, self).create(vals)
+        res.prepare_database()
+        return res
+
+    @api.one
+    def prepare_database(self):
+        registry = openerp.modules.registry.RegistryManager.get(self.name)
+
+        with registry.cursor() as cr:
+            env = Environment(cr, SUPERUSER_ID, self._context)
+            self._prepare_database(env)
+
+    @api.one
+    def _prepare_database(self, client_env):
+        # update database.uuid
+        client_env['ir.config_parameter'].set_param(cr, SUPERUSER_ID,
+                                                  'database.uuid',
+                                                  client_id)
+        # save auth data
+        oauth_provider_data = {'enabled': False, 'client_id': client_id}
+        for attr in ['name', 'auth_endpoint', 'scope', 'validation_endpoint', 'data_endpoint', 'css_class', 'body']:
+            oauth_provider_data[attr] = getattr(saas_oauth_provider, attr)
+        oauth_provider_id = client_env['auth.oauth.provider'].create(cr, SUPERUSER_ID, oauth_provider_data)
+        client_env['ir.model.data'].create(cr, SUPERUSER_ID, {
+            'name': 'saas_oauth_provider',
+            'module': 'saas_server',
+            'noupdate': True,
+            'model': 'auth.oauth.provider',
+            'res_id': oauth_provider_id,
+        })
+        # 1. Update company with organization
+        vals = {'name': organization}
+        client_env['res.company'].write(cr, SUPERUSER_ID, 1, vals)
+        partner = client_env['res.company'].browse(cr, SUPERUSER_ID, 1)
+        client_env['res.partner'].write(cr, SUPERUSER_ID, partner.id,
+                                      {'email': admin_data['email']})
+        # 2. Update user credentials
+        domain = [('login', '=', template_db)]
+        user_ids = client_env['res.users'].search(cr, SUPERUSER_ID, domain)
+        user_id = user_ids and user_ids[0] or SUPERUSER_ID
+        user = client_env['res.users'].browse(cr, SUPERUSER_ID, user_id)
+        user.write({
+            'login': admin_data['email'],
+            'name': admin_data['name'],
+            'email': admin_data['email'],
+            'parent_id': partner.id,
+            'oauth_provider_id': oauth_provider_id,
+            'oauth_uid': admin_data['user_id'],
+            'oauth_access_token': access_token
+        })
+        # 3. Set suffix for all sequences
+        seq_ids = client_env['ir.sequence'].search(cr, SUPERUSER_ID,
+                                                 [('suffix', '=', False)])
+        suffix = {'suffix': client_id.split('-')[0]}
+        client_env['ir.sequence'].write(cr, SUPERUSER_ID, seq_ids, suffix)
+        # get action_id
+        action_id = client_env['ir.model.data'].xmlid_to_res_id(cr, SUPERUSER_ID, action)
+
+        # 4. install addons
+        addons = state.get('addons')
+        if addons:
+            addon_ids = client_env['ir.module.module'].search(cr, SUPERUSER_ID, [('name', 'in', addons)])
+            for addon_id in addon_ids:
+                client_env['ir.module.module'].button_immediate_install(cr, SUPERUSER_ID, addon_id)
+    
+
 
     def update_all(self, cr, uid, server_db):
         #TODO: mark database as deleted if it not found

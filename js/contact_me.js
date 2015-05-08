510d725985aafbea8af906a453dc080c2caad850
Shikher <shikherverma@ymail.com>
2015-05-08 15:52:29 +0530

    added 7 projects

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,77 +18,9 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-				type: 'POST',
-				url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-				data: {
-					'key': 'f_9NMMrABIuJKdw_MdtxSg',
-				   'message': {
-					   'from_email': 'shikher@shikherverma.com',
-				   'to': [
-				   {
-					   'email': 'shikherverma@ymail.com',
-				   'name': 'RECIPIENT NAME (OPTIONAL)',
-				   'type': 'to'
-				   }
-				   ],
-				   'autotext': 'true',
-				   'subject': 'YOUR SUBJECT HERE!',
-				   'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
-				   }
-				}
-			}).done(function(response) {
-				console.log(response); // if you're into that sorta thing
-			});
-            /*$.ajax({
-				type: 'POST',
-				url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-				data: {
-						name: name,
-						phone: phone,
-						email: email,
-						message: message
-						'key': 'f_9NMMrABIuJKdw_MdtxSg',
-						'message': 
-							{
-								'from_email': 'shikher@shikherverma.com',
-								'to':
-								[{
-									'email': 'shikherverma@ymail.com',
-									'name': 'RECIPIENT NAME (OPTIONAL)',
-									'type': 'to'
-								}],
-								'autotext': 'true',
-								'subject': 'YOUR SUBJECT HERE!',
-								'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
-							},
-				 		success: function() 
-						{
-							// Success message
-							$('#success').html("<div class='alert alert-success'>");
-							$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
-							$('#success > .alert-success')
-							.append("<strong>Your message has been sent. </strong>");
-							$('#success > .alert-success')
-							.append('</div>');
-							//clear all fields
-							$('#contactForm').trigger("reset");
-						},
-						error: function() 
-						{
-							// Fail message
-							$('#success').html("<div class='alert alert-danger'>");
-							$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-							.append("</button>");
-							$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-							$('#success > .alert-danger').append('</div>');
-							//clear all fields
-							$('#contactForm').trigger("reset");
-						},
-				}
-                //url: "././mail/contact_me.php",
-                //type: "POST",
-                  data: {
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
                     name: name,
                     phone: phone,
                     email: email,
@@ -118,7 +50,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            })*/
+            })
         },
         filter: function() {
             return $(this).is(":visible");
@@ -130,6 +62,7 @@ $(function() {
         $(this).tab("show");
     });
 });
+
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {

d25562aa0c8cf2faf94fceafe41a710910778277
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-13 13:03:56 +0200

    add provider id if one is not presented in state

--- saas_client/controllers/main.py
+++ saas_client/controllers/main.py
@@ -3,11 +3,16 @@ import werkzeug
 from openerp import http
 from openerp.http import request
 from openerp.addons.auth_oauth.controllers import main as oauth
-
+import simplejson
 
 class SaasClient(http.Controller):
 
-    @http.route('/saas_client/new_database', type='http', auth='none')
+    @http.route(['/saas_client/new_database',
+                 '/saas_client/edit_database'], type='http', auth='none')
     def new_database(self, **post):
-        params = werkzeug.url_encode(post)
-        return werkzeug.utils.redirect('/auth_oauth/signin?%s' % params)
+        params = post.copy()
+        state = simplejson.loads(post.get('state'))
+        if not state.get('p'):
+            state['p'] = request.env.ref('saas_server.saas_oauth_provider').id
+        params['state'] = simplejson.dumps(state)
+        return werkzeug.utils.redirect('/auth_oauth/signin?%s' % werkzeug.url_encode(params))

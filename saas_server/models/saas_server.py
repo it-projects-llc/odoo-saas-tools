bb72f297446946bddcaadea1fffc895df5993945
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-12 13:15:17 +0500

    [REF] move upgrade_database method from saas_client to saas_server, because it will be easy to control access rights to upgrading

--- saas_server/models/saas_server.py
+++ saas_server/models/saas_server.py
@@ -196,3 +196,45 @@ class SaasServerClient(models.Model):
             'db_storage': db_storage,
         }
         return data
+
+    def upgrade_database(self, **post):
+        try:
+            db = request.httprequest.host.replace('.', '_')
+            pwd = config.get('tenant_passwd')
+            uid = request.session.authenticate(db, 'admin', pwd)
+            if uid:
+                module = request.registry['ir.module.module']
+                # 1. Update addons
+                update_addons = post.get('update_addons', '').split(',')
+                if update_addons:
+                    upids = module.search(request.cr, SUPERUSER_ID,
+                                          [('name', 'in', update_addons)])
+                    if upids:
+                        module.button_immediate_upgrade(request.cr, SUPERUSER_ID, upids)
+                # 2. Install addons
+                install_addons = post.get('install_addons', '').split(',')
+                if install_addons:
+                    inids = module.search(request.cr, SUPERUSER_ID,
+                                          [('name', 'in', install_addons)])
+                    if inids:
+                        module.button_immediate_install(request.cr, SUPERUSER_ID, inids)
+                # 3. Uninstall addons
+                uninstall_addons = post.get('uninstall_addons', '').split(',')
+                if uninstall_addons:
+                    unids = module.search(request.cr, SUPERUSER_ID,
+                                          [('name', 'in', uninstall_addons)])
+                    if unids:
+                        module.button_immediate_uninstall(request.cr, SUPERUSER_ID, unids)
+                # 4. Run fixes
+                fixes = post.get('fixes', '').split(',')
+                for fix in fixes:
+                    if fix:
+                        model, method = fix.split('-')
+                        getattr(request.registry[model], method)(request.cr,
+                                                                 SUPERUSER_ID)
+                status_code = 200
+            else:
+                status_code = 400
+        except:
+            status_code = 500
+        return werkzeug.wrappers.Response(status=status_code)

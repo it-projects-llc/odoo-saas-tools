5f2337f47fab1ecdba64ac777bb732ade90765b9
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-14 19:56:43 +0500

    [REF] update for new api, delete commented code, delete related field user_ids

--- oauth_provider/models/oauth_provider.py
+++ oauth_provider/models/oauth_provider.py
@@ -1,52 +1,23 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv,fields
-from openerp import SUPERUSER_ID
+from openerp import models, fields
 from datetime import datetime
-try:
-    from oauthlib.common import generate_client_id as oauthlib_generate_client_id
-except:
-    pass
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
+import uuid
 
-class oauth_application(osv.Model):
+class OauthApplication(models.Model):
     CLIENT_ID_CHARACTER_SET = r'_-.:;=?!@0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
     _name = 'oauth.application'
-    _columns = {
-        'name': fields.char('Database name', readonly=True),
-        'client_id' : fields.char('Client ID', readonly=True, select=True, required=True),
-        'token_ids': fields.one2many('oauth.access_token', 'application_id', 'Tokens'),
-        'user_ids': fields.related('token_ids', 'user_id', readonly=True, type='one2many', relation='res.users', string='Users'),
 
-        #user = models.ForeignKey(AUTH_USER_MODEL, related_name="%(app_label)s_%(class)s")
-        #help_text = _("Allowed URIs list, space separated")
-        #redirect_uris = models.TextField(help_text=help_text,
-        #                                 validators=[validate_uris], blank=True)
-        #client_type = models.CharField(max_length=32, choices=CLIENT_TYPES)
-        #authorization_grant_type = models.CharField(max_length=32,
-        #                                            choices=GRANT_TYPES)
-        #client_secret = models.CharField(max_length=255, blank=True,
-        #                                 default=generate_client_secret, db_index=True)
-        #name = models.CharField(max_length=255, blank=True)
+    def generate_client_id(self):
+        return str(uuid.uuid1())
 
-    }
-    _defaults = {
-        #'client_secret': lambda self,cr,uid,ctx={}: oauthlib_generate_client_id(length=128, chars=CLIENT_ID_CHARACTER_SET)
-    }
+    name = fields.Char('Database name', readonly=True)
+    client_id = fields.Char('Client ID', readonly=True, select=True, required=True, default=generate_client_id)
+    token_ids = fields.One2many('oauth.access_token', 'application_id', 'Tokens')
 
-#class oauth_grant(osv.Model):
-#    _name = 'oauth.access_token'
-#    _columns = {
-#        'application_id':fields.many2one('oauth.application', string='Application')
-#        'user_id':fields.many2one('res.users', string='User'),
-#        'code':fields.char('Code')
-#        'expires':fields.datetime('Expires'),
-#        'redirect_uri':fields.char('Redirect uri')
-#        'scope':fields.char('Scope')
-#    }
-
-class oauth_access_token(osv.Model):
+class OauthAccessToken(models.Model):
     _name = 'oauth.access_token'
     _columns = {
         'application_id':fields.many2one('oauth.application', string='Application'),
@@ -91,14 +62,3 @@ class oauth_access_token(osv.Model):
         for t in self.browse(cr, uid, ids, context=context):
             res[t.id] = self._allow_scopes(cr, uid, t, scopes, context=context)
         return res
-        
-#class oauth_refresh_token(osv.Model):
-#    _name = 'oauth.refresh_token'
-#    _columns = {
-#        'application_id':fields.many2one('oauth.application', string='Application')
-#        'token' : fields.char('Access Token'),
-#        'user_id':fields.many2one('res.users', string='User'),
-#        'access_token_id':fields.many2one('oauth.access_token', string='Access token'),
-#    }
-
-

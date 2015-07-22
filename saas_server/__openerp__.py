4b8d8d9aa0ef9de7a959bdfdf4e32356dde1b9eb
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-22 09:30:07 +0500

    [IMP] auth admin at saas_server via saas_portal

--- saas_server/__openerp__.py
+++ saas_server/__openerp__.py
@@ -9,6 +9,7 @@
         'views/saas_server.xml',
         'data/auth_oauth_data.xml',
         'data/ir_config_parameter.xml',
+        'data/pre_install.yml',
     ],
     'installable': True,
     'description': '''

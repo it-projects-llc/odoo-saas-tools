ff4b45415efb27c39def75cc466f854ab244ae36
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-24 15:17:40 +0500

    [IMP] delete odoo oauth provider
    [FIX] add ir_cron.xml to manifest

--- saas_client/__openerp__.py
+++ saas_client/__openerp__.py
@@ -7,7 +7,9 @@
     'depends': ['auth_oauth', 'auth_oauth_check_client_id', 'saas_utils', 'mail'],
     'data': [
         'views/saas_client.xml',
-        'security/rules.xml'
+        'security/rules.xml',
+        'data/ir_cron.xml',
+        'data/auth_oauth_data.xml',
     ],
     'installable': True,
     'description': '''

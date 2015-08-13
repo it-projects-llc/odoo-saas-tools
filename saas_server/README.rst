238fdf419ae9e55ff519c9046fb68c490d27ca06
Ivan Yelizariev <yelizariev@it-projects.info>
2015-08-13 17:08:19 +0500

    [DOC] move description to README.rst file
     delete some description, because more information for repo's README file

---
+++ saas_server/README.rst
@@ -0,0 +1,5 @@
+SaaS Server
+===========
+
+Module for central database on odoo databases cluster. Cluster is a odoo
+installation. Each cluster is located on a separate server, in the main. 
--- saas_server/__openerp__.py
+++ saas_server/__openerp__.py
@@ -12,24 +12,4 @@
         'data/pre_install.yml',
     ],
     'installable': True,
-    'description': '''
-
-Module for central database on odoo databases cluster. Cluster is a odoo
-installation. Each cluster is located on a separate server, in the main.
-
-Functions
----------
-
-* create new databases
-* copy oauth provider data to new database
-* collect information about databases
-
-Usage
-=====
-
-* You have to use --db-filter option  so that database with this module be determine unambiguously by host of request
-* install this module
-* Configure oauth provider data
-
-    ''',
 }

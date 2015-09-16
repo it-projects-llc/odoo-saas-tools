0ceab5dfba6204340d3185292c5f27cde0713051
Salton <smassally@idtlabs.sl>
2015-09-16 20:19:51 +0000

    [IMP] saas_portal_teination_protection Enable termination protection for '/saas_server/delete_database'

--- saas_portal_termination_protection/models/saas_portal.py
+++ saas_portal_termination_protection/models/saas_portal.py
@@ -5,13 +5,23 @@ from openerp import models, fields, api, SUPERUSER_ID, exceptions
 class SaasPortalClient(models.Model):
     _inherit = 'saas_portal.client'
 
-    termination_protection = fields.Boolean('Termination Protection',
+    termination_protection = fields.Boolean('Termination Protection', default=False,
                                             help='Protect from accidental deletion')
-
-    @api.multi
-    def unlink(self):
-        for client in self:
-            if client.termination_protection:
+    
+    @api.multi 
+    def _check_termination_protection(self):
+        for database in self:
+            if database.termination_protection:
                 raise exceptions.Warning('Termination Protection is enabled for client; '
                                    'If you wish to proceed please turn protection off')
+    
+    @api.multi
+    def delete_database(self):
+        self._check_termination_protection()
+        return self._request('/saas_server/delete_database')
+    
+    @api.multi
+    def unlink(self):
+        self._check_termination_protection()
         return super(SaasPortalClient, self).unlink()
+

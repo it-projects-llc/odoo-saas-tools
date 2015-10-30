c36eb1eb9587b47367ffa9c4273b09f782e4ccaa
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-30 11:05:16 +0500

    delete unused import

--- saas_portal_demo/controllers/main.py
+++ saas_portal_demo/controllers/main.py
@@ -2,7 +2,6 @@
 import werkzeug
 from openerp.addons.web import http
 from openerp.addons.web.http import request
-from openerp.addons.web.controllers.main import login_redirect
 from openerp.addons.saas_portal.controllers.main import SaasPortal
 
 

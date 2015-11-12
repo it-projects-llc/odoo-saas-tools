168a4fee0d7e1d9ad3194746f5b7f7c32444d73c
Ivan Yelizariev <yelizariev@it-projects.info>
2015-11-12 16:27:34 +0500

    [IMP] pass user_id when create client via Plan's form

--- saas_portal/models/wizard.py
+++ saas_portal/models/wizard.py
@@ -120,11 +120,17 @@ class SaasPortalCreateClient(models.TransientModel):
     name = fields.Char('Database name', required=True, default=_default_name)
     plan_id = fields.Many2one('saas_portal.plan', string='Plan', readonly=True, default=_default_plan_id)
     partner_id = fields.Many2one('res.partner', string='Partner')
+    user_id = fields.Many2one('res.users', string='User')
+
+    @api.onchange('user_id')
+    def update_parter(self):
+        if self.user_id:
+            self.partner_id = self.user_id.partner_id
 
     @api.multi
     def apply(self):
         wizard = self[0]
-        res = wizard.plan_id.create_new_database(dbname=wizard.name, partner_id=wizard.partner_id.id)
+        res = wizard.plan_id.create_new_database(dbname=wizard.name, partner_id=wizard.partner_id.id, user_id=self.user_id.id)
         client = self.env['saas_portal.client'].browse(res.get('id'))
         client.server_id.action_sync_server()
         return {

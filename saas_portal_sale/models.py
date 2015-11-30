6048863f32f18d3093cce5e2dc8d054e51b991db
Ildar Nasyrov <nasyrov@it-projects.info>
2015-11-30 09:51:31 +0500

    [FIX] _compute_expiration

--- saas_portal_sale/models.py
+++ saas_portal_sale/models.py
@@ -15,16 +15,28 @@ class ProductTemplateSaaS(models.Model):
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-    saas_portal_client_id = fields.Many2one('saas_portal.client')
+    saas_portal_client_id = fields.Many2one('saas_portal.client', string='The SaaS client this invoice was paid for')
     plan_id = fields.Many2one('saas_portal.plan', related='product_id.plan_id', readonly=True)
-    period = fields.Integer(string='Subscription period', related="product_id.period", readonly=True)
+    period = fields.Integer(string='Subscription period', help='subsciption period in days', related="product_id.period", readonly=True)
 
 
 class SaasPortalClient(models.Model):
     _inherit = 'saas_portal.client'
 
     subscription_start = fields.Datetime(string="Subscription start")
+    expiration_datetime = fields.Datetime(string="Expiration", compute='_compute_expiration', store=True)
     invoice_lines = fields.One2many('account.invoice.line', 'saas_portal_client_id')
+
+    @api.multi
+    @api.depends('invoice_lines')
+    def _compute_expiration(self):
+        for client_obj in self:
+            if client_obj.subscription_start:
+                for line in client_obj.invoice_lines:
+                    if not client_obj.expiration_datetime:
+                        client_obj.expiration_datetime = datetime.strptime(client_obj.subscription_start, DEFAULT_SERVER_DATETIME_FORMAT) + timedelta(days=line.product_id.period)
+                    else:
+                        client_obj.expiration_datetime = datetime.strptime(client_obj.expiration_datetime, DEFAULT_SERVER_DATETIME_FORMAT) + timedelta(days=line.product_id.period)
 
 
 class FindPaymentsWizard(models.TransientModel):
@@ -48,10 +60,3 @@ class FindPaymentsWizard(models.TransientModel):
     def apply_invoice_lines(self):
         client_obj = self.env['saas_portal.client'].browse(self._context.get('active_id'))
         self.invoice_lines.write({'saas_portal_client_id': client_obj.id})
-
-        if client_obj.subscription_start:
-            for line in self.invoice_lines:
-                if not client_obj.expiration_datetime:
-                    client_obj.expiration_datetime = datetime.strptime(client_obj.subscription_start, DEFAULT_SERVER_DATETIME_FORMAT) + timedelta(days=line.product_id.period)
-                else:
-                    client_obj.expiration_datetime = datetime.strptime(client_obj.expiration_datetime, DEFAULT_SERVER_DATETIME_FORMAT) + timedelta(days=line.product_id.period)

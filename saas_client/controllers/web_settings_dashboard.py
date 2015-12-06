d67110e9b33e3485336858708f203b60306d4800
Ildar Nasyrov <nasyrov@it-projects.info>
2015-12-06 12:25:06 +0500

    [ADD] usage info on dashboard

--- saas_client/controllers/web_settings_dashboard.py
+++ saas_client/controllers/web_settings_dashboard.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import openerp
 from openerp import http
 from openerp.http import request
 from openerp.addons.web_settings_dashboard.controllers.main import WebSettingsDashboard
+from openerp.addons.saas_base.tools import get_size
 
 
 class SaaSWebSettingsDashboard(WebSettingsDashboard):
@@ -15,6 +17,19 @@ class SaaSWebSettingsDashboard(WebSettingsDashboard):
         max_users = request.env['ir.config_parameter'].get_param('saas_client.max_users', default='')
         expiration_datetime = request.env['ir.config_parameter'].get_param('saas_client.expiration_datetime', default='')
 
-        result.update({'saas': {'cur_users': cur_users, 'max_users': max_users, 'expiration_datetime': expiration_datetime}})
+        data_dir = openerp.tools.config['data_dir']
+
+        file_storage = get_size('%s/filestore/%s' % (data_dir, request.env.cr.dbname))
+        file_storage = int(file_storage / (1024 * 1024))
+
+        request.env.cr.execute("select pg_database_size('%s')" % request.env.cr.dbname)
+        db_storage = request.env.cr.fetchone()[0]
+        db_storage = int(db_storage / (1024 * 1024))
+
+        result.update({'saas': {'cur_users': cur_users,
+                                'max_users': max_users,
+                                'expiration_datetime': expiration_datetime,
+                                'file_storage': file_storage,
+                                'db_storage': db_storage}})
 
         return result

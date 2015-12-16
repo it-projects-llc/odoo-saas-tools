1a33a039ea214376c8e2d46459dd5a3ab4e6a1fb
Ildar Nasyrov <nasyrov@it-projects.info>
2015-12-16 11:58:30 +0500

    [FIX] also pay_subscription_url

--- saas_client/controllers/web_settings_dashboard.py
+++ saas_client/controllers/web_settings_dashboard.py
@@ -16,7 +16,7 @@ class SaaSWebSettingsDashboard(WebSettingsDashboard):
         cur_users = request.env['res.users'].search_count([('share', '=', False)])
         max_users = request.env['ir.config_parameter'].sudo().get_param('saas_client.max_users', default='')
         expiration_datetime = request.env['ir.config_parameter'].sudo().get_param('saas_client.expiration_datetime', default='')
-        pay_subscription_url = request.env['ir.config_parameter'].get_param('saas_client.pay_subscription_url', default='').strip()
+        pay_subscription_url = request.env['ir.config_parameter'].sudo().get_param('saas_client.pay_subscription_url', default='').strip()
 
         data_dir = openerp.tools.config['data_dir']
 

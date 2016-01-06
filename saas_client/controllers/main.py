f3135ac88ad502a33e60be9bdeca6d6996095a13
Ildar Nasyrov <nasyrov@it-projects.info>
2016-01-06 17:44:38 +0500

    [ADD] customized suspended page when login

--- saas_client/controllers/main.py
+++ saas_client/controllers/main.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import werkzeug
+import openerp
 from openerp import http, SUPERUSER_ID
 from openerp.http import request
 import simplejson
-
+from openerp.addons.auth_oauth.controllers.main import OAuthLogin as Home
+from openerp.addons.web.controllers.main import ensure_db
 
 
 class SaasClient(http.Controller):
@@ -17,3 +19,15 @@ class SaasClient(http.Controller):
             state['p'] = request.env.ref('saas_server.saas_oauth_provider').id
         params['state'] = simplejson.dumps(state)
         return werkzeug.utils.redirect('/auth_oauth/signin?%s' % werkzeug.url_encode(params))
+
+
+class SaaSClientLogin(Home):
+    @http.route()
+    def web_login(self, redirect=None, **kw):
+        ensure_db()
+        param_model = request.env['ir.config_parameter']
+        suspended = param_model.sudo().get_param('saas_client.suspended', '0')
+        page_for_suspended = param_model.sudo().get_param('saas_client.page_for_suspended', '0')
+        if suspended == '1':
+            return werkzeug.utils.redirect(page_for_suspended, 303)
+        return super(SaaSClientLogin, self).web_login(redirect, **kw)

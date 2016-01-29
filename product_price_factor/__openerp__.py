d7ce4c444be557a441aab1e1ac2c8ea78fa32c3f
Ildar Nasyrov <nasyrov@it-projects.info>
2016-01-29 11:28:21 +0500

    [ADD] product_price_factor module

---
+++ product_price_factor/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product price factor",
+    'license': 'GPL-3',
+    'author': "IT-Projects LLC, Ildar Nasyrov",
+    'website': "https://twitter.com/nasyrov_ildar",
+    'category': 'Sales Management',
+    'version': '1.0.0',
+    'depends': ['product'],
+    'data': [
+        'views/product_view.xml',
+    ],
+}

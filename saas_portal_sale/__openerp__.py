67df5b918f72700902f12d7f32197c8535ad2977
Ildar Nasyrov <nasyrov@it-projects.info>
2016-02-05 12:19:27 +0500

    [FIX] error

--- saas_portal_sale/__openerp__.py
+++ saas_portal_sale/__openerp__.py
@@ -6,7 +6,7 @@
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'SaaS',
     'version': '1.0.0',
-    'depends': ['sale', 'saas_portal', 'product_price_factor'],
+    'depends': ['sale', 'saas_portal', 'product_price_factor', 'saas_portal_start'],
     'data': [
         'views/product_template.xml',
         'views/product_view.xml',

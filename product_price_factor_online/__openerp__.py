ea171c960bf5617a0bbf1fe06810c3e7364e64c4
Ildar Nasyrov <nasyrov@it-projects.info>
2016-02-16 15:47:37 +0500

    [FIX] rename product_price_factor_website to online

--- product_price_factor_online/__openerp__.py
+++ product_price_factor_online/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Product price factor website",
+    'name': "Product price factor online",
     'category': 'Website',
     'license': 'GPL-3',
     'author': "IT-Projects LLC, Ildar Nasyrov",

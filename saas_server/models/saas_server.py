3ec9a816194b5d33c834d53e584d36e61653d843
Ildar Nasyrov <nasyrov@it-projects.info>
2016-02-17 13:58:19 +0500

    [REF] saas_sysadmin_backup_agent move all code to saas_server

--- saas_server/models/saas_server.py
+++ saas_server/models/saas_server.py
@@ -1,6 +1,9 @@
 from openerp.addons.saas_base.tools import get_size
 import time
 import openerp
+import base64
+from datetime import datetime
+from openerp.service import db
 from openerp import api, models, fields, SUPERUSER_ID, exceptions
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 import psycopg2
@@ -327,3 +330,28 @@ class SaasServerClient(models.Model):
     def rename_database(self, new_dbname):
         openerp.service.db.exp_rename(self.name, new_dbname)
         self.name = new_dbname
+
+    @api.model
+    def _transport_backup(self, db_dump):
+        '''
+        backup transport agents should override this
+        '''
+        raise exceptions.Warning('Transport agent has not been configured')
+
+    @api.one
+    def backup_database(self):
+        data = {}
+        try:
+            db_dump = base64.b64decode(db.exp_dump(self.name))
+            filename = "%(db_name)s %(timestamp)s.zip" % {
+            'db_name': self.name,
+            'timestamp': datetime.utcnow().strftime(
+                "%Y-%m-%d_%H-%M-%SZ")
+            }
+            self._transport_backup(db_dump, filename=filename)
+            data['status'] = 'success'
+        except Exception, e:
+            _logger.exception('An error happened during database %s backup' %(self.name))
+            data['status'] = 'fail'
+            data['message'] = str(e)
+        return data

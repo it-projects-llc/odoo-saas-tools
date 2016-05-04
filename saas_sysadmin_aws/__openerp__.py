3bd1f37bb73c57b7d48322e72f9f95fe1143ea4c
Ildar Nasyrov <nasyrov@it-projects.info>
2016-05-04 11:33:27 +0500

    [ADD] saas_sysadmin_aws: new module

---
+++ saas_sysadmin_aws/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Saas Sysadmin AWS",
+    'author': "IT-Projects LLC, Ildar Nasyrov",
+    'license': 'LGPL-3',
+    'website': "https://twitter.com/nasyrov_ildar",
+    'category': 'SaaS',
+    'version': '1.0.0',
+    'depends': ['saas_portal'],
+    'data': [
+        'views/res_config.xml',
+    ],
+}

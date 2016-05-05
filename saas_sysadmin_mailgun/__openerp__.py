b191154112deaf7e576c8ee224504bfc7377977d
Ildar Nasyrov <nasyrov@it-projects.info>
2016-05-05 11:50:50 +0500

    [REF] saas_sysadmin_mailgun: comply with standard __openerp__.py

--- saas_sysadmin_mailgun/__openerp__.py
+++ saas_sysadmin_mailgun/__openerp__.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Saas Sysadmin Mailgun",
-    'author': "IT-Projects LLC, Ildar Nasyrov",
-    'license': 'LGPL-3',
-    'website': "https://twitter.com/nasyrov_ildar",
-    'category': 'SaaS',
-    'version': '1.0.0',
-    'depends': ['saas_sysadmin', 'saas_sysadmin_aws'],
-    'data': [
-        'views/res_config.xml',
+    "name": """Saas Sysadmin Mailgun""",
+    "summary": """This module configurates new databases for clients such as their users can send and receive mails""",
+    "category": "SaaS",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Ildar Nasyrov",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+        "saas_sysadmin_aws_route53",
     ],
+    "external_dependencies": {"python": ['boto'], "bin": []},
+    "data": [
+        "views/res_config.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
 }

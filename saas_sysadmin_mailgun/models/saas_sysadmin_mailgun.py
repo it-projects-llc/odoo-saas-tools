04f74634f4116e2f8b809230e5ce9a235fad62cc
Ildar Nasyrov <nasyrov@it-projects.info>
2016-05-05 13:01:23 +0500

    [ADD] saas_sysadmin_mailgun: implement domain creation and validation

--- saas_sysadmin_mailgun/models/saas_sysadmin_mailgun.py
+++ saas_sysadmin_mailgun/models/saas_sysadmin_mailgun.py
@@ -1,57 +1,44 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-import requests
 import simplejson
+import mailgun
 
 import logging
 _logger = logging.getLogger(__name__)
 
 try:
-    import boto
-    from boto.route53.exception import DNSServerError
+    pass
 except:
     _logger.critical('SAAS Route53 Requires the python library Boto which is not \
     found on your installation')
 
 
-
 class SaasPortalClient(models.Model):
     _inherit = 'saas_portal.client'
 
-    #TODO: create new fields for storing mailgun
-    # domain (with default value as self.name),
-    # credentials,
-    # TX validation records,
-    # MX records,
-    # CNAME record
-
-    mail_domain = fields.Char('Mail domain', default=lambda self: self.name)
+    mail_domain = fields.Char('Mail domain', default=lambda self: self.name.split('.')[0] + '.' + self.server_id.aws_hosted_zone_id.name)
 
     @api.multi
-    def _create_mail_domain_on_mailgun(self):
-        '''Creates domain on mailgun and returns smtp credentials'''
+    def _create_domain_on_mailgun(self):
+        self.ensure_one()
+        '''Creates domain on mailgun and returns domain and configuration info'''
         ir_params = self.env['ir.config_parameter']
         api_key = ir_params.get_param('saas_mailgun.saas_mailgun_api_key')
-        #TODO:
-        # 0) DONE: get api_key from ir.config_parameter
-        # 1) generate password
-        # 2) take domain name from model record.domain
-        # 3) create domain on mailgun using library
-        # 4) from return values take TX, MX, CNAME records to be created on DNS server and save them in model
-        # 5) from return values take smtp credentials and save them in model
-
+        password = 'secretpassword'
+        mailgun.add_domain(api_key=api_key, domain_name=self.mail_domain, smtp_password=password)
 
     @api.multi
-    def _route53_domain_verification_and_dns(self, domain_info):
-        ir_params = self.env['ir.config_parameter']
-        aws_access_key_id = ir_params.get_param('saas_route53.saas_route53_aws_accessid')
-        aws_secret_access_key = ir_params.get_param('saas_route53.saas_route53_aws_accesskey')
+    def _domain_verification_and_dns_route53(self, domain_info):
+        self.ensure_one()
+        receiving_dns_records = domain_info.get('receiving_dns_records')
+        name = self.mail_domain
+        for r in receiving_dns_records:
+            value = r.get('priority') + ' ' + r.get('value') + '\n'
+        self.server_id._update_zone(name=name, type='mx', value=value)
 
-        #TODO:
-        # 0) DONE: get access_key_id and secrets_access_key from ir.config_parameter
-        # 1) get records to be created from model
-        # 2) create the records on Amazon Route53 server
-
+        sending_dns_records = domain_info.get('sending_dns_records')
+        for r in sending_dns_records:
+            self.server_id._update_zone(name=r['name'], type=r['record_type'].lower, value=r['value'])
 
 
 class SaasPortalPlan(models.Model):
@@ -62,10 +49,11 @@ class SaasPortalPlan(models.Model):
         res = super(SaasPortalPlan, self)._create_new_database(**kw)
 
         client_obj = self.env['saas_portal.client'].browse(res.get('id'))
-        res = client_obj._create_mail_domain_on_mailgun()
+        res = client_obj._create_domain_on_mailgun()
         new_domain_info = simplejson.loads(res)
         client_obj._route53_domain_verification_and_dns(new_domain_info)
+
         ir_params = self.env['ir.config_parameter']
         api_key = ir_params.get_param('saas_mailgun.saas_mailgun_api_key')
-        client_obj.upgrade(payload={'configure_outgoing_mail': smtp_credentials, 'params': [{'key': 'saas_client.mailgun_api_key', 'value': api_key, 'hidden': True}]})
+        client_obj.upgrade(payload={'configure_outgoing_mail': new_domain_info['domain'], 'params': [{'key': 'saas_client.mailgun_api_key', 'value': api_key, 'hidden': True}]})
         return res

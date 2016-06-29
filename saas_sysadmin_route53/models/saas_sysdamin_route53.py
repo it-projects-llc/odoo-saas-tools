563a3081b7c022d6816b0251914be3eb40c37b2e
Ivan Yelizariev <yelizariev@it-projects.info>
2016-06-29 14:41:17 +0500

    [FIX] aws_hosted_zone was renamed to aws_hosted_zone_id

--- saas_sysadmin_route53/models/saas_sysdamin_route53.py
+++ saas_sysadmin_route53/models/saas_sysdamin_route53.py
@@ -13,7 +13,7 @@ class SaasPortalPlan(models.Model):
     def create_template(self):
         assert len(self) == 1, 'This method is applied only for single record'
         plan = self[0]
-        if plan.server_id.aws_hosted_zone:
+        if plan.server_id.aws_hosted_zone_id:
             plan.server_id._update_zone(plan.template_id.name, value=plan.server_id.name, type='cname') 
         return super(SaasPortalPlan, self).create_template()
     
@@ -21,7 +21,7 @@ class SaasPortalPlan(models.Model):
     def delete_template(self):
         super(SaasPortalPlan, self).delete_template()
         plan = self[0]
-        if plan.server_id.aws_hosted_zone:
+        if plan.server_id.aws_hosted_zone_id:
             plan.server_id._update_zone(plan.template_id.name, type='cname', action='delete')
         return True         
     
@@ -32,7 +32,7 @@ class SaasPortalClient(models.Model):
     @api.returns('self', lambda value:value.id)
     def create(self, vals):
         client = super(SaasPortalClient, self).create(vals)
-        if client.server_id.aws_hosted_zone:
+        if client.server_id.aws_hosted_zone_id:
             client.server_id._update_zone(client.name, value=client.server_id.name, type='cname')            
         return client
     
@@ -41,7 +41,7 @@ class SaasPortalClient(models.Model):
         for client in self:
             if 'server_id' in vals:
                 server = self.env['saas_portal.server'].browse(vals['server_id'])
-                if client.server_id.aws_hosted_zone and server.id != client.server_id.id:
+                if client.server_id.aws_hosted_zone_id and server.id != client.server_id.id:
                     client.server_id._update_zone(client.name, value=client.server_id.name, 
                                                   type='cname', action='update') 
         super(SaasPortalClient, self).write(vals)    
@@ -50,6 +50,6 @@ class SaasPortalClient(models.Model):
     @api.multi
     def unlink(self):        
         for client in self:
-          if client.server_id.aws_hosted_zone:
+          if client.server_id.aws_hosted_zone_id:
             client.server_id._update_zone(client.name, type='cname', action='delete')  
         return super(SaasPortalClient, self).unlink()

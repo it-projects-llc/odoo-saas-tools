e8cc2640c98f2a25be7e71e76e398d8cc74a0ee9
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-08 11:28:23 +0500

    [PORT] saas_sysadmin_mailgun: res.config.settings 10.0

--- saas_sysadmin_mailgun/models/res_config.py
+++ saas_sysadmin_mailgun/models/res_config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaasPortalConfigWizard(models.TransientModel):
@@ -7,10 +7,12 @@ class SaasPortalConfigWizard(models.TransientModel):
 
     saas_mailgun_api_key = fields.Char('Mailgun API Key')
 
-    def get_default_saas_mailgun_api_key(self):
+    @api.model
+    def get_default_saas_mailgun_api_key(self, fields):
         saas_mailgun_api_key = self.env["ir.config_parameter"].get_param("saas_mailgun.saas_mailgun_api_key", default=None)
         return {'saas_mailgun_api_key': saas_mailgun_api_key or False}
 
+    @api.multi
     def set_saas_mailgun_api_key(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:

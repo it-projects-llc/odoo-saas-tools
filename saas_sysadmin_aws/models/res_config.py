db6b4f5bf6eabb8fbc4a0e725f5ea05770774f66
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-08 11:22:27 +0500

    [PORT] saas_sysadmin_aws: res.config.settings for 10.0

--- saas_sysadmin_aws/models/res_config.py
+++ saas_sysadmin_aws/models/res_config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaasPortalConfigWizard(models.TransientModel):
@@ -8,19 +8,23 @@ class SaasPortalConfigWizard(models.TransientModel):
     saas_route53_aws_accessid = fields.Char('AWS Access ID')
     saas_route53_aws_accesskey = fields.Char('AWS Secret Key')
 
-    def get_default_saas_route53_aws_accessid(self):
+    @api.model
+    def get_default_saas_route53_aws_accessid(self, fields):
         saas_route53_aws_accessid = self.env["ir.config_parameter"].get_param("saas_route53.saas_route53_aws_accessid", default=None)
         return {'saas_route53_aws_accessid': saas_route53_aws_accessid or False}
 
+    @api.multi
     def set_saas_route53_aws_accessid(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_route53.saas_route53_aws_accessid", record.saas_route53_aws_accessid or '')
 
-    def get_default_saas_route53_aws_accesskey(self):
+    @api.model
+    def get_default_saas_route53_aws_accesskey(self, fields):
         saas_route53_aws_accesskey = self.env["ir.config_parameter"].get_param("saas_route53.saas_route53_aws_accesskey", default=None)
         return {'saas_route53_aws_accesskey': saas_route53_aws_accesskey or False}
 
+    @api.multi
     def set_saas_route53_aws_accesskey(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:

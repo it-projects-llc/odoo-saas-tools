6519902bbbda30880b2072f0490e5e008cb17166
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-08 11:31:37 +0500

    [PORT] saas_server_backup_s3: res.config.settings 10.0

--- saas_server_backup_s3/models/res_config.py
+++ saas_server_backup_s3/models/res_config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SaasPortalConfigWizard(models.TransientModel):
@@ -9,28 +9,34 @@ class SaasPortalConfigWizard(models.TransientModel):
     saas_s3_aws_accesskey = fields.Char('AWS Secret Key')
     saas_s3_aws_bucket = fields.Char('S3 Bucket')
 
-    def get_default_saas_s3_aws_accessid(self):
+    @api.model
+    def get_default_saas_s3_aws_accessid(self, fields):
         saas_s3_aws_accessid = self.env["ir.config_parameter"].get_param("saas_s3.saas_s3_aws_accessid", default=None)
         return {'saas_s3_aws_accessid': saas_s3_aws_accessid or False}
 
+    @api.multi
     def set_saas_s3_aws_accessid(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_s3.saas_s3_aws_accessid", record.saas_s3_aws_accessid or '')
 
-    def get_default_saas_s3_aws_accesskey(self):
+    @api.model
+    def get_default_saas_s3_aws_accesskey(self, fields):
         saas_s3_aws_accesskey = self.env["ir.config_parameter"].get_param("saas_s3.saas_s3_aws_accesskey", default=None)
         return {'saas_s3_aws_accesskey': saas_s3_aws_accesskey or False}
 
+    @api.multi
     def set_saas_s3_aws_accesskey(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_s3.saas_s3_aws_accesskey", record.saas_s3_aws_accesskey or '')
 
-    def get_default_saas_s3_aws_bucket(self):
+    @api.model
+    def get_default_saas_s3_aws_bucket(self, fields):
         saas_s3_aws_bucket = self.env["ir.config_parameter"].get_param("saas_s3.saas_s3_aws_bucket", default=None)
         return {'saas_s3_aws_bucket': saas_s3_aws_bucket or False}
 
+    @api.multi
     def set_saas_s3_aws_bucket(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:

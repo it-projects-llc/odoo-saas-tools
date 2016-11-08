ba3fb812c463348e4d3980111a7497bff202a2fd
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-08 11:11:16 +0500

    [PORT] saas_portal: res.config uses new get_default methods for 10.0

--- saas_portal/models/res_config.py
+++ saas_portal/models/res_config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 import urlparse
 
 
@@ -18,7 +18,8 @@ class SaasPortalConfigWizard(models.TransientModel):
     module_saas_portal_sale_online = fields.Boolean(string='Sale SaaS from website shop', help='Use saas_portal_sale_online module')
 
     # base_saas_domain
-    def get_default_base_saas_domain(self):
+    @api.model
+    def get_default_base_saas_domain(self, fields):
         base_saas_domain = self.env["ir.config_parameter"].get_param("saas_portal.base_saas_domain", default=None)
         if base_saas_domain is None:
             domain = self.env["ir.config_parameter"].get_param("web.base.url")
@@ -28,46 +29,55 @@ class SaasPortalConfigWizard(models.TransientModel):
                 pass
         return {'base_saas_domain': base_saas_domain or False}
 
+    @api.multi
     def set_base_saas_domain(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_portal.base_saas_domain", record.base_saas_domain or '')
 
     # page_for_maximumdb
-    def get_default_page_for_maximumdb(self):
+    @api.model
+    def get_default_page_for_maximumdb(self, fields):
         page_for_maximumdb = self.env["ir.config_parameter"].get_param("saas_portal.page_for_maximumdb", default='/')
         return {'page_for_maximumdb': page_for_maximumdb or '/'}
 
+    @api.multi
     def set_page_for_maxumumdb(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_portal.page_for_maximumdb", record.page_for_maximumdb or '/')
 
     # page_for_maximumtrialdb
-    def get_default_page_for_maximumtrialdb(self):
+    @api.model
+    def get_default_page_for_maximumtrialdb(self, fields):
         page_for_maximumtrialdb = self.env["ir.config_parameter"].get_param("saas_portal.page_for_maximumtrialdb", default='/')
         return {'page_for_maximumtrialdb': page_for_maximumtrialdb or '/'}
 
+    @api.multi
     def set_page_for_maxumumtrialdb(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_portal.page_for_maximumtrialdb", record.page_for_maximumtrialdb or '/')
 
     # page_for_nonfree_subdomains
-    def get_default_page_for_nonfree_subdomains(self):
+    @api.model
+    def get_default_page_for_nonfree_subdomains(self, fields):
         page_for_nonfree_subdomains = self.env["ir.config_parameter"].get_param("saas_portal.page_for_nonfree_subdomains", default='/')
         return {'page_for_nonfree_subdomains': page_for_nonfree_subdomains or '/'}
 
+    @api.multi
     def set_page_for_nonfree_subdomains(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:
             config_parameters.set_param("saas_portal.page_for_nonfree_subdomains", record.page_for_nonfree_subdomains or '/')
 
     # expiration_notify_in_advance
-    def get_default_expiration_notify_in_advance(self):
+    @api.model
+    def get_default_expiration_notify_in_advance(self, fields):
         expiration_notify_in_advance = self.env["ir.config_parameter"].get_param("saas_portal.expiration_notify_in_advance", default='0')
         return {'expiration_notify_in_advance': expiration_notify_in_advance or '0'}
 
+    @api.multi
     def set_expiration_notify_in_advance(self):
         config_parameters = self.env["ir.config_parameter"]
         for record in self:

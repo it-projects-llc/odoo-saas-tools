549b9551811b5d584aa30dbfe673fbdb887778d7
Ildar Nasyrov <nasyrov@it-projects.info>
2016-11-17 17:34:27 +0500

    Revert "[ADD] saas_portal: create templates with addons"
    This reverts commit 0a17268fe4d4503084e2e1120c34b7e7d97aa01b.

--- saas_portal/models/saas_portal.py
+++ saas_portal/models/saas_portal.py
@@ -317,13 +317,13 @@ class SaasPortalPlan(models.Model):
         return self.dbname_template.replace('%i', sequence)
 
     @api.multi
-    def create_template(self, addons=None):
+    def create_template(self):
         assert len(self) == 1, 'This method is applied only for single record'
         plan = self[0]
         state = {
             'd': plan.template_id.name,
             'demo': plan.demo and 1 or 0,
-            'addons': addons or [],
+            'addons': [],
             'lang': plan.lang,
             'tz': plan.tz,
             'is_template_db': 1,

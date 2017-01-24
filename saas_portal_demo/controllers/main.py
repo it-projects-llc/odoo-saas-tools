793f87216636dde8418220ec546fcedf76541fdb
iledarn <nasyrov@it-projects.info>
2017-01-24 17:52:50 +0500

    [ADD] saas_portal_demo: user maximum version available if no version defined for saas demo product

--- saas_portal_demo/controllers/main.py
+++ saas_portal_demo/controllers/main.py
@@ -34,10 +34,16 @@ class website_sale_custom(website_sale):
     @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
     def product(self, product, category='', search='', **kwargs):
         version = kwargs.get('version')
+        attr_id = request.env.ref('saas_portal_demo.odoo_version_product_attribute').id
         if version:
-            attr_id = request.env.ref('saas_portal_demo.odoo_version_product_attribute').id
             var_id = request.env['product.attribute.value'].search([('attribute_id', '=', attr_id),('name', '=', version)]).id
             url = request.httprequest.url.split('?', 1)[0] + '?attrib=%s-%s' % (attr_id, var_id)
+            return werkzeug.utils.redirect(url)
+        if product.saas_demo and not '?' in request.httprequest.url:
+            attr_vals = product.product_variant_ids.mapped('attribute_value_ids').filtered(lambda r: r.attribute_id.id == attr_id)
+            vals_dict = {val: int(val.split('.', 1)[0]) for val in attr_vals.mapped('name')}
+            max_version = max(vals_dict, key=vals_dict.get)
+            url = request.httprequest.url + '?version=' + max_version
             return werkzeug.utils.redirect(url)
 
         return super(website_sale_custom, self).product(product=product, category=category, search=search, **kwargs)

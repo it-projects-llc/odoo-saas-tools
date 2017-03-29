8f9d1992c363e3935e99ac1d87c45a3f7ea6abe5
iledarn <nasyrov@it-projects.info>
2017-03-29 10:00:25 +0500

    [ADD] saas_portal_signup_custom: default saas product option

--- saas_portal_signup_custom/models/product.py
+++ saas_portal_signup_custom/models/product.py
@@ -8,3 +8,4 @@ class ProductTemplate(models.Model):
     plan_ids = fields.Many2many('saas_portal.plan',
                                 string='SaaS Plans',
                                 help='Create db per each selected plan - use the DB Names prefix setting in each selected plans')
+    saas_default = fields.Boolean('Use as default SaaS product')

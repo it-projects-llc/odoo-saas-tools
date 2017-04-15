53823cdaf33a7c803a72a9d11509ae275afd9f8c
iledarn <nasyrov@it-projects.info>
2017-04-15 12:02:16 +0500

    [FIX] saas_portal_signup_custom2: post requests data format

--- saas_portal_signup_custom2/models/website.py
+++ saas_portal_signup_custom2/models/website.py
@@ -15,10 +15,12 @@ class Website(models.Model):
 
     @api.multi
     def recaptcha_siteverify(self, response):
+        print '\n\n\n', 'response', response, '\n\n\n'
         for record in self:
+            print '\n\n\n', 'secret key', record.google_recaptcha_secret_key, '\n\n\n'
+            data = simplejson.dumps({'secret': record.google_recaptcha_secret_key, 'response': response})
             req_res = requests.post('https://www.google.com/recaptcha/api/siteverify',
-                                    data={'secret': record.google_recaptcha_secret_key,
-                                          'response': response})
+                                    data=data)
             res = simplejson.loads(req_res.content)
             print '\n\n\n', 'reCAPTCHA res', res, '\n\n\n'
             return res.get('success')

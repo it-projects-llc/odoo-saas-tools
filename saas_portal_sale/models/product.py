c8aac867eaaa29629efb0ae1ed23f7967f24a8b7
iledarn <nasyrov@it-projects.info>
2017-04-19 16:18:22 +0500

    [ADD] saas_portal_sale: product template may have many plans, plan may have one product

--- saas_portal_sale/models/product.py
+++ saas_portal_sale/models/product.py
@@ -5,7 +5,13 @@ from odoo import models, fields
 class ProductTemplateSaaS(models.Model):
     _inherit = 'product.template'
 
-    plan_id = fields.Many2one('saas_portal.plan', string='Plan')
+    plan_id = fields.Many2one('saas_portal.plan', string='Plan')  # obsolete
+
+    plan_ids = fields.One2many('saas_portal.plan', 'product_tmpl_id',
+                                string='SaaS Plans',
+                                help='Create db per each selected plan - use the DB Names prefix setting in each selected plans')
+    saas_default = fields.Boolean('Is default', help='Use as default SaaS product on signup form')
+    on_create_email_template = fields.Many2one('mail.template', string='credentials mail')
 
 
 class ProductAttributeSaaS(models.Model):
@@ -18,12 +24,6 @@ class ProductAttributeSaaS(models.Model):
                 ('MAX_USERS', 'MAX_USERS'),
                 ('INSTALL_MODULES', 'INSTALL_MODULES'),
                 ('STORAGE_LIMIT', 'STORAGE_LIMIT')]
-    # saas_code = fields.Char('SaaS code', help='''Possible codes:
-    # * SUBSCRIPTION_PERIOD
-    # * MAX_USERS
-    # * INSTALL_MODULES
-    # * STORAGE_LIMIT
-    # ''')
 
 
 class ProductAttributeValueSaaS(models.Model):

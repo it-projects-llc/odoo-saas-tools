6d25992852bc8737e5cd0a67a4054ff8d7089844
Ivan Yelizariev <yelizariev@it-projects.info>
2017-05-01 16:06:04 +0500

    fix company name

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Odoo SaaS Tools'
-copyright = u'IT-Projects LLC.'
+copyright = u'IT-Projects LLC'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

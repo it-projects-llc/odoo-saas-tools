d60328a5d6c55f4d090f702acfdabc52031f704f
iledarn <nasyrov@it-projects.info>
2017-05-03 16:08:57 +0500

    [FIX] docs/conf.py: 10.0 version here

--- docs/conf.py
+++ docs/conf.py
@@ -53,9 +53,9 @@ copyright = u'IT-Projects LLC'
 # built documents.
 #
 # The short X.Y version.
-version = '9.0'
+version = '10.0'
 # The full version, including alpha/beta/rc tags.
-release = '9.0'
+release = '10.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:

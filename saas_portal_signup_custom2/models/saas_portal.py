8209e1460fe7c1517dbd8b4b8a1fd23dc20cdb44
iledarn <nasyrov@it-projects.info>
2017-05-25 18:59:53 +0500

    [FIX] saas_portal_signup_custom2: store form data in client instance's res_partner

--- saas_portal_signup_custom2/models/saas_portal.py
+++ saas_portal_signup_custom2/models/saas_portal.py
@@ -2,6 +2,7 @@
 
 from odoo import fields
 from odoo import models
+from odoo import api
 
 
 class SaasPortalPlan(models.Model):
@@ -11,3 +12,19 @@ class SaasPortalPlan(models.Model):
                                 help='specify this field if you want to create several databases at once using saas_portal_signup module',
                                 placeholder='test-')
     on_create = fields.Selection(default='home')
+
+    @api.multi
+    def _prepare_owner_user_data(self, owner_user, owner_password):
+        owner_user_data = super(SaasPortalPlan, self)._prepare_owner_user_data(owner_user, owner_password)
+        owner_user_data.update({
+            'company_name': owner_user.company_name,
+            'website': owner_user.website,
+            'phone': owner_user.phone,
+            'fax': owner_user.fax,
+            'city': owner_user.city,
+            'street': owner_user.street,
+            'vat': owner_user.vat,
+            'zip': owner_user.zip,
+            'country_id': owner_user.country_id.id,
+        })
+        return owner_user_data

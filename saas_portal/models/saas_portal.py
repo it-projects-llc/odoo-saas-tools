4ea1c23b5fe0ff6a1ba0dde64d240283101f52e9
iledarn <nasyrov@it-projects.info>
2017-07-03 15:41:27 +0000

    [FIX] saas_portal: something wrong with this in 8.0

--- saas_portal/models/saas_portal.py
+++ saas_portal/models/saas_portal.py
@@ -196,8 +196,7 @@ class SaasPortalPlan(models.Model):
     on_create = fields.Selection([
         ('login', 'Log into just created instance'),
     ], string="Workflow on create", default='login')
-    on_create_email_template = fields.Many2one('mail.template',
-                                               default=lambda self: self.env.ref('saas_portal.email_template_create_saas'))
+    on_create_email_template = fields.Many2one('mail.template')
 
     @api.one
     @api.depends('template_id.state')

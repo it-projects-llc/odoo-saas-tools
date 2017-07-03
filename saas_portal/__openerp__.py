625a6eab8ca6028d2619d479482d57eee3876a64
iledarn <nasyrov@it-projects.info>
2017-07-03 11:15:41 +0000

    [REF] saas_portal: it was wrong approach with information page. Bettere use messages in an saas instance or `/my/home` (portal) page

--- saas_portal/__openerp__.py
+++ saas_portal/__openerp__.py
@@ -22,8 +22,6 @@
         'views/res_users.xml',
         'data/res_users.xml',
         'data/base_action_rule.xml',
-        'views/try_trial_template.xml',
-        'views/information_template.xml',
         'security/ir.model.access.csv',
     ],
     'installable': True,

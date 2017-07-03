c8ba4bc798643c42c202cc60b9be30f76b49441b
iledarn <nasyrov@it-projects.info>
2017-07-03 17:35:33 +0000

    [IMP] saas_portal_signup_custom2: `Domain exists` error message

--- saas_portal_signup_custom2/controllers/main.py
+++ saas_portal_signup_custom2/controllers/main.py
@@ -106,7 +106,7 @@ class AuthSignupHome(auth_signup.controllers.main.AuthSignupHome):
             full_dbname = f_dbname.replace('www.', '')
             db_exists = odoo.service.db.exp_db_exist(full_dbname)
             assert re.match('[a-zA-Z0-9_.-]+$', qcontext.get('dbname')), "Only letters or numbers are allowed in domain."
-            assert db_exists == False, "Domain exists"
+            assert db_exists == False, "Web address exists, please choose another web address"
         assert re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", values['email']), "Please enter a valid email address."
         assert any([k for k in values.values()]), "The form was not properly filled in."
         assert values.get('password') == qcontext.get('confirm_password'), "Passwords do not match; please retype them."

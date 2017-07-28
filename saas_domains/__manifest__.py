49ffcb34c88c61ac614f3d8ca559bf85b01b2003
Cristian Salamea <ovnicraft@gmail.com>
2017-07-28 09:45:09 -0500

    [IMP] module to manage DNS zones

---
+++ saas_domains/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'SaaS Sysadmin DNS Manager',
+    'version': '10.0.1.0.0',
+    'author': 'Cristian Salamea',
+    'license': 'LGPL-3',
+    'category': 'SaaS',
+    "support": "apps@it-projects.info",
+    'website': 'https://it-projects.info',
+    'depends': ['saas_sysadmin'],
+    'data': [
+        'views/domain_views.xml'
+    ],
+    'installable': True,
+}

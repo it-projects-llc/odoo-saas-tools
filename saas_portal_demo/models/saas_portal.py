a84f6eb32cd1294ab4c2ce3d89ff8edd69dc217f
Ildar Nasyrov <nasyrov@it-projects.info>
2017-10-09 12:52:27 +0000

    [ADD] saas_portal_demo: if instance is created for a user then make home action for this user to open /my/instances

--- saas_portal_demo/models/saas_portal.py
+++ saas_portal_demo/models/saas_portal.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, SUPERUSER_ID
+
+
+class SaasPortalPlan(models.Model):
+    _inherit = 'saas_portal.plan'
+
+    @api.multi
+    def create_new_database(self, **kwargs):
+        res = super(SaasPortalPlan, self).create_new_database(**kwargs)
+        user_id = kwargs.get('user_id')
+        user = user_id and user_id != SUPERUSER_ID and self.env['res.users'].browse(user_id)
+        if user:
+            user.action_id = self.env.ref('saas_portal_demo.action_open_my_instances').id
+        return res

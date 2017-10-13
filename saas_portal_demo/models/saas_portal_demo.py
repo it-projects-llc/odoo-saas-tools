6c02aceb2a627c9a5771c70b5aee579eac7e54c8
Ildar <nasyrov@it-projects.info>
2017-10-13 14:15:59 +0500

    [IMP] saas_server_demo: allow to store demo images anywhere inside module tree - not restrict to `static/description/demo` (#659)
    * [TEXT] saas_portal_demo: prices comment

--- saas_portal_demo/models/saas_portal_demo.py
+++ saas_portal_demo/models/saas_portal_demo.py
@@ -119,11 +119,11 @@ class SaasPortalServer(models.Model):
                 'website_published': True,
                 'seo_url': demo_module.get('demo_url'),
                 'description': demo_module.get('demo_summary'),
-                'image': images_res and images_res.popitem()[1] or None,
+                'image': images_res and images_res.pop(0)[1] or None,
                 'sale_on_website': False,
                 'saas_demo': True,
                 'type': 'service',
-                'product_image_ids': images_res and [(0, 0, {'name': name, 'image': image}) for name, image in images_res.iteritems()] or None,
+                'product_image_ids': images_res and [(0, 0, {'name': name, 'image': image}) for name, image in images_res] or None,
             }
             product_template = product_template_obj.with_context({
                 'create_product_product': False

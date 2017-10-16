cff86c20abb7b78353db9ce60aea93e95406c8fc
Ildar Nasyrov <nasyrov@it-projects.info>
2017-10-16 06:58:19 +0000

    [ADD] account_custom2_security: new module that makes available `Settings` for `Accounting` - for saas_portal_signup_custom2

---
+++ account_custom2_security/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Account custom2 security""",
+    "summary": """Make Settings menu in Accounting available to group_erp_manager (Access Rights)""",
+    "category": "Accounting",
+    # "live_test_url": "",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ildar Nasyrov",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/iledarn",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "sale",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "data/account_accountant_data.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}

d761f1b00ea09ada1ccb8d66f105b9d5a3c6d907
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-11 14:40:30 +0000

    [REM] saas_portal_sale: remove old subsctiption management approach

--- saas_portal_sale/models/saas_portal.py
+++ saas_portal_sale/models/saas_portal.py
@@ -10,40 +10,33 @@ class SaasPortalPlan(models.Model):
     free_subdomains = fields.Boolean(help='allow to choose subdomains for trials otherwise allow only after payment', default=True)
     non_trial_instances = fields.Selection([('from_trial', 'From trial'), ('create_new', 'Create new')], string='Non-trial instances',
                                            help='Whether to use trial database or create new one when user make payment', required=True, default='create_new')
+    product_tmpl_id = fields.Many2one('product.template', 'Product')
+
+    product_variant_ids = fields.One2many('product.product', 'saas_plan_id', 'Product variants')
 
     @api.multi
     def _new_database_vals(self, vals):
         vals = super(SaasPortalPlan, self)._new_database_vals(vals)
-        vals['contract_id'] = self.env['account.analytic.account'].sudo().create({
+        product = self.env['product.product'].browse(vals['product_id'])
+        partner = self.env['res.partner'].browse(vals['partner_id'])
+        pricelist = partner.property_product_pricelist and partner.property_product_pricelist.id or False
+
+        contract = self.env['account.analytic.account'].sudo().create({
             'name': vals['name'],
             'partner_id': vals['partner_id'],
             'recurring_invoices': True,
-        }).id
+            'pricelist_id': partner.property_product_pricelist and partner.property_product_pricelist.id or False,
+            'recurring_invoice_line_ids': [(0, 0, {
+                'product_id': product.id,
+                'name': product.name_get()[0][1],
+                'price_unit': partner.property_product_pricelist and product.with_context(pricelist=partner.property_product_pricelist.id).price or 0.0,
+                'uom_id': product.uom_id.id,
+            })],
+        })
+        contract.cron_recurring_create_invoice()  # create invoice for new database immediately
+
+        vals['contract_id'] = contract.id
         return vals
-
-
-    @api.multi
-    def create_new_database(self, **kwargs):
-        res = super(SaasPortalPlan, self).create_new_database(**kwargs)
-
-        partner_id = kwargs.get('partner_id')
-        trial = kwargs.get('trial')
-        if not partner_id:
-            return res
-        if trial and self.non_trial_instances != 'from_trial':
-            return res
-
-        lines = self.env['saas_portal.subscription_wizard'].find_partner_payments(partner_id=partner_id, plan_id=self.id)
-        client_obj = self.env['saas_portal.client'].browse(res.get('id'))
-        lines.write({'saas_portal_client_id': client_obj.id})
-
-        if not trial:
-            client_obj.subscription_start = client_obj.create_date
-
-        payload = client_obj.get_upgrade_database_payload()
-        self.env['saas.config'].do_upgrade_database(payload, client_obj)
-
-        return res
 
 
 class SaasPortalClient(models.Model):

7207056077121e935c259454cc1db4ded2870b63
Ildar Nasyrov <nasyrov@it-projects.info>
2018-01-12 11:09:17 +0000

    [FIX] saas_portal_sale: autovalidate recurring invoices??? - comment out for now. 'Please create some invoice lines.'

--- saas_portal_sale/models/account_analytic_account.py
+++ saas_portal_sale/models/account_analytic_account.py
@@ -11,9 +11,10 @@ class AccountAnalyticAccount(models.Model):
     def _create_invoice(self):
         self.ensure_one()
         invoice = super(AccountAnalyticAccount, self)._create_invoice()
-        invoice.action_invoice_open()  # validate the invoice
-        if invoice.contract_id:
-            client = self.env['saas_portal.client'].search([('contract_id', '=', invoice.contract_id.id)], limit=1)
+        # Autovalidate recurring invoices??? There is no line somehow. But super must return the invoice with one line! There is account.invoice.line create either.
+        # invoice.action_invoice_open()  # validate the invoice
+        # if invoice.contract_id:
+        #     client = self.env['saas_portal.client'].search([('contract_id', '=', invoice.contract_id.id)], limit=1)
 
         return invoice
 

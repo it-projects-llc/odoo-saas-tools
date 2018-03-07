d4cb2ca2d816cc9251e1d37d5551568d734be734
Nicolas JEUDY <njeudy@panda-chi.io>
2018-03-07 14:05:21 +0100

    [NEW] product_price_factor and product_price_factor_online V11 ready

--- product_price_factor/models/product.py
+++ product_price_factor/models/product.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 import odoo.addons.decimal_precision as dp
 
@@ -25,7 +24,8 @@ class ProductAttributeValue(models.Model):
             return
 
         p_obj = self.env['product.attribute.price']
-        p_ids = p_obj.search([('value_id', '=', id), ('product_tmpl_id', '=', active_id)])
+        p_ids = p_obj.search(
+            [('value_id', '=', id), ('product_tmpl_id', '=', active_id)])
         if p_ids:
             p_ids.write({'price_factor': value})
         else:
@@ -36,14 +36,15 @@ class ProductAttributeValue(models.Model):
             })
 
     price_factor = fields.Float(compute="_get_price_factor", string='Attribute Price Factor',
-                                        inverse=_set_price_factor,
+                                        inverse="_set_price_factor",
                                         digits=dp.get_precision('Product Price'))
 
 
 class ProductAttributePrice(models.Model):
     _inherit = "product.attribute.price"
 
-    price_factor = fields.Float('Price Factor', digits=dp.get_precision('Product Price'), default=1.0)
+    price_factor = fields.Float(
+        'Price Factor', digits=dp.get_precision('Product Price'), default=1.0)
 
 
 class ProductProduct(models.Model):
@@ -56,14 +57,16 @@ class ProductProduct(models.Model):
         if not uom and self._context.get('uom'):
             uom = self.env['product.uom'].browse(self._context['uom'])
         if not currency and self._context.get('currency'):
-            currency = self.env['res.currency'].browse(self._context['currency'])
+            currency = self.env['res.currency'].browse(
+                self._context['currency'])
 
         products = self
         if price_type == 'standard_price':
             # standard_price field can only be seen by users in base.group_user
             # Thus, in order to compute the sale price from the cost for users not in this group
             # We fetch the standard price as the superuser
-            products = self.with_context(force_company=company and company.id or self._context.get('force_company', self.env.user.company_id.id)).sudo()
+            products = self.with_context(force_company=company and company.id or self._context.get(
+                'force_company', self.env.user.company_id.id)).sudo()
 
         prices = dict.fromkeys(self.ids, 0.0)
         for product in products:
@@ -72,15 +75,18 @@ class ProductProduct(models.Model):
                 for line in product.product_tmpl_id.attribute_line_ids:
                     for value in product.attribute_value_ids.filtered(lambda r: r.attribute_id == line.attribute_id):
                         for price in value.price_ids.filtered(lambda r: r.product_tmpl_id == product.product_tmpl_id):
-                            prices[product.id] = (prices[product.id] + price.price_extra) * price.price_factor
+                            prices[product.id] = (
+                                prices[product.id] + price.price_extra) * price.price_factor
 
             if uom:
-                prices[product.id] = product.uom_id._compute_price(prices[product.id], uom)
+                prices[product.id] = product.uom_id._compute_price(
+                    prices[product.id], uom)
 
             # Convert from current user company currency to asked one
             # This is right cause a field cannot be in more than one currency
             if currency:
-                prices[product.id] = product.currency_id.compute(prices[product.id], currency)
+                prices[product.id] = product.currency_id.compute(
+                    prices[product.id], currency)
 
         return prices
 
@@ -97,4 +103,5 @@ class ProductAttributeLine(models.Model):
         num = self.search_count([]) + 1
         return num
 
-    sequence = fields.Integer('Sequence', help="Determine the display order", required=True, default=_default_sequence)
+    sequence = fields.Integer(
+        'Sequence', help="Determine the display order", required=True, default=_default_sequence)

e9038691f60f2cfd266590e8623cc64b4e20dc9f
Nicolas JEUDY <njeudy@panda-chi.io>
2018-03-09 12:07:01 +0100

    [MIG] backup s3 V11 ready

--- saas_server_backup_s3/models/res_config.py
+++ saas_server_backup_s3/models/res_config.py
@@ -1,43 +1,28 @@
-# -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
 
 class SaasPortalConfigWizard(models.TransientModel):
-    _inherit = 'saas_server.config.settings'
+    _inherit = 'res.config.settings'
 
     saas_s3_aws_accessid = fields.Char('AWS Access ID')
     saas_s3_aws_accesskey = fields.Char('AWS Secret Key')
     saas_s3_aws_bucket = fields.Char('S3 Bucket')
 
-    @api.model
-    def get_default_saas_s3_aws_accessid(self, fields):
-        saas_s3_aws_accessid = self.env["ir.config_parameter"].sudo().get_param("saas_s3.saas_s3_aws_accessid", default=None)
-        return {'saas_s3_aws_accessid': saas_s3_aws_accessid or False}
-
     @api.multi
-    def set_saas_s3_aws_accessid(self):
-        config_parameters = self.env["ir.config_parameter"]
-        for record in self:
-            config_parameters.set_param("saas_s3.saas_s3_aws_accessid", record.saas_s3_aws_accessid or '')
+    def set_values(self):
+        super(SaasServerWizard, self).set_values()
+        ICPSudo = self.env['ir.config_parameter'].sudo()
+        ICPSudo.set_param("saas_s3.saas_s3_aws_accessid", str(int(self.saas_s3_aws_accessid)))
+        ICPSudo.set_param("saas_s3.saas_s3_aws_accesskey", self.saas_s3_aws_accesskey)
+        ICPSudo.set_param("saas_s3.saas_s3_aws_bucket", self.saas_s3_aws_bucket)
 
     @api.model
-    def get_default_saas_s3_aws_accesskey(self, fields):
-        saas_s3_aws_accesskey = self.env["ir.config_parameter"].sudo().get_param("saas_s3.saas_s3_aws_accesskey", default=None)
-        return {'saas_s3_aws_accesskey': saas_s3_aws_accesskey or False}
-
-    @api.multi
-    def set_saas_s3_aws_accesskey(self):
-        config_parameters = self.env["ir.config_parameter"]
-        for record in self:
-            config_parameters.set_param("saas_s3.saas_s3_aws_accesskey", record.saas_s3_aws_accesskey or '')
-
-    @api.model
-    def get_default_saas_s3_aws_bucket(self, fields):
-        saas_s3_aws_bucket = self.env["ir.config_parameter"].sudo().get_param("saas_s3.saas_s3_aws_bucket", default=None)
-        return {'saas_s3_aws_bucket': saas_s3_aws_bucket or False}
-
-    @api.multi
-    def set_saas_s3_aws_bucket(self):
-        config_parameters = self.env["ir.config_parameter"]
-        for record in self:
-            config_parameters.set_param("saas_s3.saas_s3_aws_bucket", record.saas_s3_aws_bucket or '')
+    def get_values(self):
+        res = super(SaasPortalConfigWizard, self).get_values()
+        ICPSudo = self.env['ir.config_parameter'].sudo()
+        res.update(
+            saas_s3_aws_accesskey=ICPSudo.get_param('saas_s3.saas_s3_aws_accesskey'),
+            saas_s3_aws_bucket=ICPSudo.get_param('saas_s3.saas_s3_aws_bucket'),
+            saas_s3_aws_accessid=ICPSudo.get_param('saas_s3.saas_s3_aws_accessid'),
+        )
+        return res

a7c3ed7d9c5a80e685ec03098a6952d34e140226
Nicolas JEUDY <njeudy@panda-chi.io>
2018-05-20 12:21:58 +0200

    [FIX] res_config default get migrate

--- saas_client/models/res_config.py
+++ saas_client/models/res_config.py
@@ -9,15 +9,15 @@ class BaseConfigSettings(models.TransientModel):
     current_domain = fields.Char(readonly=True)
     domain_change_link = fields.Html(readonly=True)
 
-    # current_domain
     @api.model
-    def get_default_current_domain(self, fields):
-        current_domain = self.env["ir.config_parameter"].sudo().get_param('web.base.url', default=None)
-        return {'current_domain': current_domain or False}
-
-    # domain_change_link
-    @api.model
-    def get_default_domain_change_link(self, fields):
+    def get_values(self):
+        res = super(BaseConfigSettings, self).get_values()
+        ICPSudo = self.env['ir.config_parameter'].sudo()
+        current_domain = ICPSudo.get_param('saas_client.current_domain', default=ICPSudo.get_param('web.base.url'))
         link = self.env["ir.config_parameter"].sudo().get_param('saas_client.saas_dashboard', default=None)
         html = link and '<a href=' + link + '>' + 'You can change your domain name here' + '</a>' or False
-        return {'domain_change_link': html}
+        res.update(
+            current_domain=current_domain,
+            domain_change_link=html,
+        )
+        return res

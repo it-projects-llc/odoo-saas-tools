7f0404ff005155a1a81efadcb927a64d5b607fbe
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2018-12-13 17:51:45 +0100

    [12.0] [MIG] saas_server_backup_ftp

--- saas_server_backup_ftp/__manifest__.py
+++ saas_server_backup_ftp/__manifest__.py
@@ -5,12 +5,12 @@
     "support": "apps@it-projects.info",
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'SaaS',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': ['saas_server'],
     "external_dependencies": {"python": ['pysftp'], "bin": []},
     'data': [
         'views/res_config.xml',
         'data/ir_cron.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
